a83b1527ff9c3094f07399a19cc52c3e7d458532
Indiras Khatri <42945839+Kindiras@users.noreply.github.com>
2021-04-25 18:27:41 -0400

    Add files via upload

--- Vectors/VectorInClass/2dvector.h
+++ Vectors/VectorInClass/2dvector.h
@@ -1,1 +1,22 @@
 
+#ifndef VECTOR_2D
+#define VECTOR_2D
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <ctime>    
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+class RandomSpin
+{
+public:
+    double random_spin();
+private:
+    vector< vector<int> > grid;
+};
+#endif
---
+++ Vectors/VectorInClass/2dvector_spin.cpp
@@ -0,0 +1,7 @@
+#include "2dvector.h"
+
+double RandomSpin::RandomSpin::random_spin()
+    {
+        int randomNumber = rand();
+    return float(randomNumber)/RAND_MAX;
+    }    
