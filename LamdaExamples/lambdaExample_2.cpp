2bc6e1e68577711327741dbcebd52b49f9757844
Indiras Khatri <42945839+Kindiras@users.noreply.github.com>
2021-10-14 20:42:15 -0400

    Add files via upload

---
+++ LamdaExamples/lambdaExample_2.cpp
@@ -0,0 +1,28 @@
+//
+//  lambdaExample_2.cpp
+//  LamdaExpression
+//
+//  Created by Indiras Khatri on 10/14/21.
+//  Copyright © 2021 Indiras Khatri. All rights reserved.
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int main ()
+{
+    //creating a vector of integers
+    vector<int> vectorInts;
+    //creating an iterator for the vector
+    vector<int>::iterator it;
+    
+    vectorInts.push_back(0);
+    vectorInts.push_back(1);
+    vectorInts.push_back(2);
+    vectorInts.push_back(3);
+    vectorInts.push_back(4);
+    vectorInts.push_back(5);
+    vectorInts.push_back(6);
+    vectorInts.push_back(7);
+}
